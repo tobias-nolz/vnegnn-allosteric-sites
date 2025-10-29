d9f1693d6f197a3c0bb3cf19e0914268e54fef54
Tobias Nolz <git@tobias-nolz.at>
2025-10-29 13:21:27 +0100

    changes to main to execute embedding generation and binding info extraction

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ from pathlib import Path
 
 from prepare_pdb import prepare_pdb_directory
 from prepare_ligand import prepare_ligands_from_asd
+from generate_embeddings import generate_embeddings
+from extract_binding_info import extract_binding_info
 
-from process_pdb import prepare_pdb_directory
 
 def setup_data(pdb_dir: Path, asd_dataset: pd.DataFrame, vnegnn_path: Path):
     pdb_ids = asd_dataset['allosteric_pdb'].tolist()
@@ -21,8 +22,30 @@ def setup_data(pdb_dir: Path, asd_dataset: pd.DataFrame, vnegnn_path: Path):
     prepare_ligands_from_asd(
         pdb_dir=pdb_dir,
         ligand_info=ligand_info,
-        check_existing=False,
+        skip_existing=True,
         workers=16
+    )
+
+    generate_embeddings(
+        pdb_path=PDB_DIR,
+        vnegnn_path=vnegnn_path,
+        workers=4,
+        batch_size=8,
+        monitor_memory=True,
+        verbose=True,
+        output_format="npz",
+        check_if_exists=True
+    )
+
+    extract_binding_info(
+        pdb_path=PDB_DIR,
+        vnegnn_path=VNEGNN_PATH,
+        threshold=4.0,
+        workers=4,
+        verbose=True,
+        backend="processes",
+        msms_path=msms_path,
+        check_if_exists=True
     )
 
 
@@ -35,7 +58,12 @@ if __name__ == "__main__":
     VNEGNN_PATH = Path(os.path.abspath(os.path.join("..", "..", "vnegnn")))
 
     asd_dataset = pd.read_csv(ASD_FILE, sep="\t")
+    asd_dataset = asd_dataset.sample(n=8, random_state=42).reset_index(drop=True)
 
-    # Setup the data directory with PDB files and ligands
-    setup_data(pdb_dir=PDB_DIR, asd_dataset=asd_dataset.iloc[:5], vnegnn_path=VNEGNN_PATH)
+    setup_data(
+        pdb_dir=PDB_DIR,
+        asd_dataset=asd_dataset.iloc[:32],
+        vnegnn_path=VNEGNN_PATH,
+        msms_path=MSMS_PATH
+    )
 
